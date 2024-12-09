5c915c1bdf3c11b934aac52e82c5c9f78b7a1421
lsx2216 <1607140832@qq.com>
2024-12-09 12:10:11 +0800

    first commit

---
+++ 第4阶段-LESSON/JB4-2-公共服务/image/LessonSearchApp.java
@@ -0,0 +1,12 @@
+package com.lsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author lsx */
+@SpringBootApplication
+public class LessonSearchApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LessonSearchApp.class, args);
+    }
+}
---
+++ 第4阶段-LESSON/JB4-4-后台后端/image/LessonAdminApp.java
@@ -0,0 +1,12 @@
+package com.lsx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author lsx */
+@SpringBootApplication
+public class LessonAdminApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LessonAdminApp.class, args);
+    }
+}
